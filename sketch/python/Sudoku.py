45087ee8dc57546df1c7a465e495a1b030c9518d
JanvRuij <jan.vanruijven96@gmail.com>
2024-04-30 15:08:21 +0200

    symetrieee

--- sketch/python/Sudoku.py
+++ sketch/python/Sudoku.py
@@ -1,46 +1,31 @@
-z = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [2, 3, 0, 5, 6, 7, 8, 9, 1],
-    [3, 4, 0, 6, 7, 8, 9, 1, 2],
-    [4, 5, 0, 7, 8, 9, 1, 2, 3],
-    [0, 6, 7, 8, 9, 1, 2, 3, 4],
-    [6, 7, 8, 9, 1, 2, 3, 4, 5],
-    [7, 8, 9, 1, 2, 3, 4, 5, 6],
-    [8, 9, 1, 2, 3, 4, 5, 6, 7],
-    [9, 1, 2, 3, 4, 5, 6, 7, 8]
-]
-contains_0 = True
+import numpy as np
+from basis import find_xyz, find_symmetric_point, check_symmetry
 
-while(contains_0):
-    zT = list(zip(*z))
-    x_z_combos = []
-    y_z_combos = []
-    for i in range(9):
-        for j in range(1, 10):
-            x_z_combos.append([i, j])
-            y_z_combos.append([i, j])
+z = np.matrix([
+    [3, 4, 1, 2],
+    [2, 1, 3, 4],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+        ])
 
-    for i in range(9):
-        for j in z[i]:
-            if j != 0:
-                x_z_combos.remove([i, j])
 
-    for i in range(9):
-        for j in zT[i]:
-            if j != 0:
-                y_z_combos.remove([i, j])
+def Solve(lq):
+    points = np.empty((0, 2))
+    for index in np.ndindex(lq.shape):
+        if lq[index] != 0:
+            points = np.vstack(
+                    (points,
+                     np.array([index[0] - lq[index], index[1] - lq[index]]))
+                    )
 
-    for p in x_z_combos:
-        for q in y_z_combos:
-            if p[1] == q[1]:
-                z[p[0]][q[0]] = q[1]
+    print(check_symmetry(points))
+    points = np.vstack((points, find_symmetric_point(points)))
+    print(check_symmetry(points))
 
-    has0 = False
-    for i in range(9):
-        for j in range(9):
-            if z[i][j] == 0:
-                has0 = True
-    contains_0 = has0
+    points = np.vstack((points, find_symmetric_point(points)))
+    print(check_symmetry(points))
+    print(lq)
+    print(points)
 
-print(z)
-print(z)
+
+Solve(z)
---
+++ sketch/python/basis.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def check_symmetry(points):
+    symmetric = True
+    for point in points:
+        reflection = - point
+        # Check if the reflection exists in the set of points
+        if not any(np.all(reflection == p) for p in points):
+            symmetric = False
+            break
+    return symmetric
+
+
+def find_xyz(arr, possible_x, possible_y):
+    result = []
+    for x, z in possible_x:
+        for y, z_ in possible_y:
+            if x - z == arr[0] and y - z_ == arr[1]:
+                result.append((x, y, z))
+    return result
